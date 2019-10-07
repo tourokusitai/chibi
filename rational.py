849004c650f85240e776bbecc30749ade899a0a9
SHIMIZU Ami <21716048@jwu.ac.jp>
2019-10-07 16:15:24 +0900

    add inf

--- rational.py
+++ rational.py
@@ -1,10 +1,12 @@
 class Q(object):
-    def __init__(self,a,b):
+    def __init__(self,a,b=1):
         self.a = a
         self.b = b
     def __repr__(self):
+        if self.b == 1:
+            return str (self.a)
         return f'{self.a}/{self.b}'
 
 
-q = Q(1,2)
+q = Q(3)
 print(q)
