7114c998ecf82a54cb3e584029b051dac45cebf2
SHIMIZU Ami <21716048@jwu.ac.jp>
2019-10-07 16:52:27 +0900

    add add

--- rational.py
+++ rational.py
@@ -14,7 +14,24 @@ class Q(object):
         c = q.a
         d = q.b
         return Q(a*d+b*c,b*d)
-     
+    def __sub__(self,q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*d-b*c,b*d)
+    def __mul__(self,q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*c,b*d)
+    def __truediv__(self,q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*d,b*c)   
 
 q1 = Q(1,2)
 q2 = Q(1,3)
