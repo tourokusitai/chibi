4c84b343beba5a11c4372389dc635bbdfedfb39b
SHIMIZU Ami <21716048@jwu.ac.jp>
2019-10-07 16:50:51 +0900

    add add add

--- rational.py
+++ rational.py
@@ -1,7 +1,9 @@
+import math
 class Q(object):
     def __init__(self,a,b=1):
-        self.a = a
-        self.b = b
+        gcd = math.gcd(a,b)
+        self.a = a//gcd
+        self.b = b//gcd
     def __repr__(self):
         if self.b == 1:
             return str (self.a)
@@ -12,7 +14,11 @@ class Q(object):
         c = q.a
         d = q.b
         return Q(a*d+b*c,b*d)
+     
 
 q1 = Q(1,2)
 q2 = Q(1,3)
 print(q1 + q2)#==>5/6
+print(q1 - q2)#==>1/6
+print(q1 * q2)#==>1/6
+print(q1 / q2)#==>3/2
