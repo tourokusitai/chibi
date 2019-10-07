4887e199ff43a6d6d3be237b8e0ea16c4b7b0292
SHIMIZU Ami <21716048@jwu.ac.jp>
2019-10-07 15:45:43 +0900

    Counter class

---
+++ counter.py
@@ -0,0 +1,25 @@
+class Counter(object):
+    def __init__(self):#コンストラクタ
+        self.cnt = 0
+
+    def count(self):
+        self.cnt +=1
+
+    def doublecount(self):
+        self.cnt +=2
+
+    def reset(self):
+        self.cnt = 0
+
+    def show(self):
+        print(self.cnt)
+
+    def __repr__(self): #文字列を返すと表示される
+        return str(self.cnt)
+
+c =Counter()
+c.show()
+c.doublecount()
+c.show()
+print(type(c))
+print(c)
