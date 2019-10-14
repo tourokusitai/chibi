856df4d54f88fe32fa01ca997ac55927ab7163be
SHIMIZU Ami <21716048@jwu.ac.jp>
2019-10-14 16:44:11 +0900

    Expr

--- exp.py
+++ exp.py
@@ -1,6 +1,9 @@
 
-class Val(object):
-    __slot__ = ['value']
+class Expr(object):
+    pass
+
+class Val(Expr):
+    __slots__ = ['value']
     def __init__(self, value = 0):
             self.value = value
     def __repr__(self):
@@ -12,4 +15,30 @@ v = Val(1)
 print(v)
 assert v.eval() == 1
 
+assert isinstance(v, Expr)#==>True
+assert isinstance(v, Val)#==>True
+assert not isinstance(v, int)
+
+def toExpr(a):
+    if not isinstance(a,Expr):
+        a = Val(a)
+    return a
+
+class Add(Expr):
+    __slots__=['left','right']
+    def __init__(self,a,b):
+        self.left = toExpr(a)
+        self.right =toExpr(b)
+    def eval(self):
+        return self.left.eval() + self.right.eval()
+
+
+
+e = Add(1,Add(1,2))
+print(e.eval())
+assert e.eval() == 4
+
+
 print()
+print()
+
