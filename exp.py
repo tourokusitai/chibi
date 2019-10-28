1123beb62872029025dcb1fde6270a648967fdde
SHIMIZU Ami <21716048@jwu.ac.jp>
2019-10-28 16:15:01 +0900

    parser

--- exp.py
+++ exp.py
@@ -5,40 +5,67 @@ class Expr(object):
 class Val(Expr):
     __slots__ = ['value']
     def __init__(self, value = 0):
-            self.value = value
+        self.value = value
     def __repr__(self):
         return f'Val({self.value})'
     def eval(self):
         return self.value
 
 v = Val(1)
-print(v)
+#print(v)
 assert v.eval() == 1
 
-assert isinstance(v, Expr)#==>True
-assert isinstance(v, Val)#==>True
-assert not isinstance(v, int)
-
 def toExpr(a):
-    if not isinstance(a,Expr):
-        a = Val(a)
+    if not isinstance(a, Expr):
+            a = Val(a)
     return a
 
-class Add(Expr):
-    __slots__=['left','right']
-    def __init__(self,a,b):
-        self.left = toExpr(a)
-        self.right =toExpr(b)
+class Binary(Expr):
+    def __repr__(self):
+        cname = self.__class__.__name__
+        return f'{cname}({self.left},{self.right})'
+
+class Add(Binary):
+    __slots__=['left', 'right']
+    def __init__(self, a, b):
+        self.left = toExpr(a)   # aとb は式
+        self.right = toExpr(b)
     def eval(self):
         return self.left.eval() + self.right.eval()
 
-
-
 e = Add(1,Add(1,2))
-print(e.eval())
+#print(e)
 assert e.eval() == 4
 
+e = Add(Val(1),Add(Val(2),Val(3)))
+assert e.eval() == 6
 
-print()
-print()
+class Sub(Binary):
+    __slots__=['left', 'right']
+    def __init__(self, a, b):
+        self.left = toExpr(a)   # aとb は式
+        self.right = toExpr(b)
+    def eval(self):
+        return self.left.eval() - self.right.eval()
+
+class Mul(Binary):
+    __slots__=['left', 'right']
+    def __init__(self, a, b):
+        self.left = toExpr(a)   # aとb は式
+        self.right = toExpr(b)
+    def eval(self):
+        return self.left.eval() * self.right.eval()
+
+class Div(Binary):
+    __slots__=['left', 'right']
+    def __init__(self, a, b):
+        self.left = toExpr(a)   # aとb は式
+        self.right = toExpr(b)
+    def eval(self):
+        return self.left.eval() // self.right.eval()
+
+
+
+
+
 
--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ from exp import Val,Add
 def parse(s:str):
     num = int(s)
     return Val(num)
-
+'''
 e = parse("123")
 print(e)
 
@@ -14,4 +14,18 @@ print('pos',pos)
 s1 = s[0:pos]
 s2 = s[pos+1:]
 print(s,s1,s2)
+'''
+
+def parse(s: str):
+    pos = s.find('+')
+    if pos == -1:
+        num = int(s)
+        return Val(num)
+    else:
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(Val(int(s1)), Val(int(s2)))
+
+e = parse("123+456")
+print(e)
 
