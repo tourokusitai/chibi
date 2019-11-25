f385638c416888926f6c717655701702f65c8867
SHIMIZU Ami <21716048@jwu.ac.jp>
2019-11-25 16:45:18 +0900

    starting calc.py

--- icalc.py
+++ icalc.py
@@ -1,26 +1,26 @@
 import pegpy
 
 peg = pegpy.grammar('''
-Expression = Product(^{'+' Product #Add})*
-Product = Value(^{'*' Value #Mul})*
+Expression = Product(^{'+' Product #Add })*
+Product = Value(^{'*' Value #Mul })*
 Value = {[0-9]+ #Int}
 ''')
-
 parser = pegpy.generate(peg)
-
-t = parser('1+2*3')
-print(repr(t))
 
 def calc(t):
     if t == 'Int':
         return int(str(t))
     if t == 'Add':
-        return clac(t[0]) + calc(t[1])
+        return calc(t[0]) + calc(t[1])
     if t == 'Mul':
         return calc(t[0]) * calc(t[1])
-    print(f'TODO{t.tag}')
+    print(f'TODO {t.tag}')
     return 0
 
-t = parser('1+2*3+4*5')
-print(repr(t))
-prinut(calc(t))
+def main():
+    s = input('$ ')
+    t = parser(s)
+    print(calc(t))
+
+if __name__ == '__main__':
+    main()
