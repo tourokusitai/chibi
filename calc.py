775470e0c0631ced63ec0662349f3a9c35f6acb2
SHIMIZU Ami <21716048@jwu.ac.jp>
2020-01-15 14:43:52 +0900

    tabetai

--- calc.py
+++ calc.py
@@ -6,3 +6,4 @@ p=[1,2,3,4,5]
 t=[1,2,3,4,5,6,7,8,9,10,11]
 d=[1,2,3,4,5,6,7,8,9,10]
 print('sarada=',random.choice(s),'zensai=',random.sample(z,2),'niku=',random.choice(n),'pizza=',random.choice(p),'tansuikabutu=',random.choice(t),'dezart=',random.choice(d))
+ 
