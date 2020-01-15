535e75c0a3a859c26b2e5161d097bf2bbda9e494
SHIMIZU Ami <21716048@jwu.ac.jp>
2020-01-15 14:33:18 +0900

    saizesaize

--- calc.py
+++ calc.py
@@ -1,7 +1,8 @@
 import random
 s=[1,2,3,4,5]
-z=[1,2,3,4,5]
+z=[1,2,3,4,5,6,7,8,9,10]
 n=[1,2,3,4,5]
-t=[1,2,3,4,5,6,7,8,9,10]
-d=[1,2,3,4,5,6]
-print('sarada=',random.choice(s),'zensai=',random.sample(z,2),'niku=',random.choice(n),'tansuikabutu=',random.choice(t),'dezart=',random.choice(d))
+p=[1,2,3,4,5]
+t=[1,2,3,4,5,6,7,8,9,10,11]
+d=[1,2,3,4,5,6,7,8,9,10]
+print('sarada=',random.choice(s),'zensai=',random.sample(z,2),'niku=',random.choice(n),'pizza=',random.choice(p),'tansuikabutu=',random.choice(t),'dezart=',random.choice(d))
