669f9fde90795b0e94f261f5429989edb2f9be03
SHIMIZU Ami <21716048@jwu.ac.jp>
2020-01-15 14:07:59 +0900

    saize

--- calc.py
+++ calc.py
@@ -1,9 +1,7 @@
-
-def calc(s):
-    print('s=',s)
-    nums = map(int,s.split('+'))
-    print('nums=',nums)
-    return sum(nums)
-print(calc("1"))
-print(calc("1+2"))
-print(calc("1+2+3"))
+import random
+s=[1,2,3,4,5]
+z=[1,2,3,4,5]
+n=[1,2,3,4,5]
+t=[1,2,3,4,5,6,7,8,9,10]
+d=[1,2,3,4,5,6]
+print('sarada=',random.choice(s),'zensai=',random.sample(z,2),'niku=',random.choice(n),'tansuikabutu=',random.choice(t),'dezart=',random.choice(d))
