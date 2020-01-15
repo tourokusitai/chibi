bff10b2ab470603beb0308ae253b7027205bf7c8
SHIMIZU Ami <21716048@jwu.ac.jp>
2020-01-15 14:18:58 +0900

    aa

--- calc.py
+++ calc.py
@@ -5,4 +5,3 @@ n=[1,2,3,4,5]
 t=[1,2,3,4,5,6,7,8,9,10]
 d=[1,2,3,4,5,6]
 print('sarada=',random.choice(s),'zensai=',random.sample(z,2),'niku=',random.choice(n),'tansuikabutu=',random.choice(t),'dezart=',random.choice(d))
-  
